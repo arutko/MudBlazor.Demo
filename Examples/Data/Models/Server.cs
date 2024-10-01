9aff61835548abd51418ac885db037243a11ede4
Adam Rutkowski <agrutko@outlook.com>
2024-10-02 00:48:42 +0200

    make demo controls usable

---
+++ Examples/Data/Models/Server.cs
@@ -0,0 +1,9 @@
+﻿namespace MudBlazor.Demo.BW.Examples.Data.Models;
+
+public class Server
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Location { get; set; }
+    public string IpAddress { get; set; }
+}
