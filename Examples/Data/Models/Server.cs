4063d5402b3150c1d255f73f976add0c7f750b59
Adam Rutkowski <agrutko@outlook.com>
2024-10-02 23:29:19 +0200

    Added MudBlazor.Demo.Components Added MudBlazor.Demo.BA Added MudBlazor.Demo.Hybrid

--- Examples/Data/Models/Server.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MudBlazor.Demo.BW.Examples.Data.Models;
-
-public class Server
-{
-    public Guid Id { get; set; }
-    public string Name { get; set; }
-    public string Location { get; set; }
-    public string IpAddress { get; set; }
-}
